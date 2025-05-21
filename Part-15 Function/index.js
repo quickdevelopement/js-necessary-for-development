7334628d66f536e4a4c35a84f43b6fbe224aa0ba
ariful4082 <ariful4082@gmail.com>
2025-05-21 09:01:14 +0600

    javascript object

---
+++ Part-15 Function/index.js
@@ -0,0 +1,17 @@
+function nameCall(a, b){
+    // return "Mr." + name;
+    // console.log("Mr.", name)
+
+    let sum = a + b;
+    console.log(sum)
+};
+
+
+
+// let arif = nameCall("Ariful Islam");
+
+// console.log(arif);
+
+// nameCall("Johirul Islam")
+
+nameCall(25,6);
---
+++ Part-16 Object/index.js
@@ -0,0 +1,16 @@
+// literal way
+
+let car = {
+    name:'Corola',
+    brand:'Volovo',
+    model:500,
+    color: 'red'
+};
+
+console.log(car);
+
+// Constructor Way
+
+let person = new Object({name:'Ariful', age:30});
+console.log(person)
+
